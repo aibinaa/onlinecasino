d7b55a268abee81ef85b9837ca3dab9e68bbd7ff
Aibina <aibina@Aibinas-MacBook-Air.local>
2022-06-10 21:07:55 +0600

    next

--- static/main.js
+++ static/main.js
@@ -2,9 +2,5 @@ const playBtn = document.querySelector('.play-btn')
 const input = document.querySelector('.input')
 
 playBtn.addEventListener('click', () => {
-    // alert(input.value)
-    alert('you loose')
-    alert('sss')
-    alert('aaaa')
-    alert('aa')
+    alert(input.value)
 });
