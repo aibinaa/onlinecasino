25f0b8e93612a37c520a271c8e613f5dae2ec79e
Aibina <aibina@Aibinas-MacBook-Air.local>
2022-06-22 21:24:19 +0600

    added randomizer

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-// const http = require('http')
-// var server = http.createServer((request, responce) => {
-//     responce.writeHead(200);
-//     responce.end("Hello http")
-// });
-// server.listen(7777, () => {
-//     console.log('server listens port')
-// })
+const express = require('express');
+const randomizer = require('./randomizer');
 
-const { response } = require('express')
-const { request } = require('express')
-const express = require('express')
-const randomizer = require('./randomizer')
-// const randomizer = require('./randomizer')
-// console.log(randomizer(5))
-const app = new express()
+const app = new express();
 
-app.use(express.json())
+app.use(express.json());
+
+const responseObj = {};
+
+app.post('/name', (req, res) => {
+    responseObj.name = req.body.name;
+});
 
 app.post('/user', (request, response) => {
     let randomNumber = randomizer()
-    if(request.body.userNumber === randomNumber) {
-        console.log(`User number: ${request.body.userNumber}, random number: ${randomNumber} You won`);
+    if (+request.body.userNumber === randomNumber) {
+        responseObj.message = `${responseObj.name}, you won!`;
+    } else {
+        responseObj.message = `${responseObj.name}, you lost!`;
     }
-    else {
-        console.log(`user number: ${request.body.userNumber}, random number: ${randomNumber} you lost`)
-    }
-    // console.log(request.body.userNumber)
-})
-app.use(express.static('static')).listen(9999, () => {
-    console.log('express server listen port 9999')
-})
+    response.send(JSON.stringify(responseObj));
+});
 
-// const randomizer = require('./randomizer')
-// console.log(randomizer(5))
+app.use(express.static('static')).listen(9999, () => {
+    console.log('Express server listens on port 9999');
+});
--- static/main.js
+++ static/main.js
@@ -1,12 +1,44 @@
-const playBtn = document.querySelector('.play-btn')
-const userNumber = document.querySelector('.user-number')
+const playBtn = document.querySelector('.play-btn');
+const userInput = document.querySelector('.user-input');
+const resultHeader = document.querySelector('.result-header');
+
+const namePrompt = prompt('Enter your name');
+
+const nameObj = {
+    name: namePrompt
+};
+
+fetch('/name', {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(nameObj)
+});
 
 playBtn.addEventListener('click', () => {
-    // alert(userNumber.value)
-    const obj = { userNumber: userNumber.value }
-    fetch('/user', {
-        method: 'POST',
-        body: JSON.stringify(obj),
-        headers: { 'Content-Type': 'application/json'}
-    })
+    if (userInput.value === '') {
+        alert('Empty string. Enter a number!')
+    } else if (isNaN(userInput.value)) {
+        alert('Enter a number!');
+    } else if (Number(userInput.value) > 5 || Number(userInput.value) < 0) {
+        alert('Enter a number between 0 and 5');
+    } else {
+        const obj = {
+            userNumber: userInput.value
+        };
+
+        fetch('/user', {
+            method: 'POST',
+            headers: {
+                 'Content-Type': 'application/json'
+                },
+            body: JSON.stringify(obj)
+        }).then(response => response.json())
+          .then(response => {
+            console.log(response);
+            resultHeader.innerHTML = response.message
+          })
+          .then()
+    }
 });
